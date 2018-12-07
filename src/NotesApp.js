1c6c577fb09bd4e13b9da043944021bb4fd0683a
Aylin DeBruyne <adebruyne88@gmail.com>
2018-12-07 14:55:59 -0500

    adds helper function to locate a note by its id

--- src/NotesApp.js
+++ src/NotesApp.js
@@ -7,16 +7,20 @@ class NotesApp extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      currentNoteId: 1002,
       notes: [
         {
+          id: 1001,
           title: "note 1",
           content: "pew pew pew"
         },
         {
+          id: 1002,
           title: "note 2",
           content: "lalalala"
         },
         {
+          id: 1003,
           title: "note 3",
           content: "chicka dee dee dee dee dee dee deeeeee"
         }
@@ -28,10 +32,20 @@ class NotesApp extends Component {
       <div className="notes-app container">
         <SearchBar />
         <NoteList notes={this.state.notes.map(note => note.title)} />
-        <EditorWindow content={"it was the best of times"} />
+        <EditorWindow
+          content={this._getById(this.state.currentNoteId).content}
+        />
       </div>
     );
   }
+
+  _getById = idToFind => {
+    //find the object in this.state.notes where id === idToFind
+    const theOne = this.state.notes.find(note => {
+      return note.id === idToFind;
+    });
+    return theOne;
+  };
 }
 
 export default NotesApp;
