1ae874caeba8e4ff52f4a0e1178e3ec8969d4a38
Aylin DeBruyne <adebruyne88@gmail.com>
2018-12-07 13:33:49 -0500

    adds dummy smaller components

---
+++ src/NotesApp.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class NotesApp extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div />;
+  }
+}
+
+export default NotesApp;
