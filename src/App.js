b6997da2720e33342bf8049bcc48f3d014a81b21
Aylin DeBruyne <adebruyne88@gmail.com>
2018-12-07 13:27:49 -0500

    create initial layout for app hardcode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="notes-app">
+          <div className="searchbar">
+            <input
+              value="it"
+              type="text
+        "
+            />
+          </div>
+          <div className="notes-list">
+            <ul>
+              <li>
+                <a href="">note 1</a>
+              </li>
+              <li>
+                <a href="">note 2</a>
+              </li>
+              <li>
+                <a href="">note 3</a>
+              </li>
+            </ul>
+          </div>
+          <div className="editor-window">"This is a note"</div>
+        </div>
       </div>
     );
   }
